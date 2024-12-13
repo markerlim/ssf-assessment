3a1fa025a21125969eeb99e01f0756427e4d545e
Marcus Lim <markerlim15@gmail.com>
2024-12-13 13:59:08 +0800

    modified to make noticeboard.api modular

--- src/main/java/vttp/batch5/ssf/noticeboard/services/NoticeService.java
+++ src/main/java/vttp/batch5/ssf/noticeboard/services/NoticeService.java
@@ -3,6 +3,7 @@ package vttp.batch5.ssf.noticeboard.services;
 import java.net.URI;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
@@ -12,15 +13,14 @@ import org.springframework.web.client.RestTemplate;
 import jakarta.json.JsonObject;
 import vttp.batch5.ssf.noticeboard.repositories.NoticeRepository;
 
-
 @Service
 public class NoticeService {
 
 	@Autowired
 	private NoticeRepository noticeRepository;
 
-	private static final String BASE_URL = "https://publishing-production-d35a.up.railway.app/";
-
+	@Value("${noticeboard.api}")
+	private String BASE_URL;
 
 	// TODO: Task 3
 	// You can change the signature of this method by adding any number of
@@ -28,11 +28,16 @@ public class NoticeService {
 	// and return any type
 	public String postToNoticeServer(JsonObject jsonObject) {
 		RestTemplate restTemplate = new RestTemplate();
-		String url = BASE_URL + "notice";
+		String url = "";
+		if (BASE_URL.isEmpty()) {
+			url = "https://publishing-production-d35a.up.railway.app/notice";
+		} else {
+			url = BASE_URL;
+		}
 
 		RequestEntity<String> request = RequestEntity
 				.post(URI.create(url))
-                .contentType(MediaType.APPLICATION_JSON)
+				.contentType(MediaType.APPLICATION_JSON)
 				.body(jsonObject.toString());
 
 		ResponseEntity<String> response = restTemplate.exchange(request, String.class);
@@ -40,12 +45,11 @@ public class NoticeService {
 
 	}
 
-
-	public void insertNotices(String payload){
+	public void insertNotices(String payload) {
 		noticeRepository.insertNotices(payload);
 	}
 
-	public String randomKey(){
+	public String randomKey() {
 		return noticeRepository.randomKey();
 	}
 }
